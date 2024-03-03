bb9d284c0d09162975d84878e000be316cd6a806
janahmedprg <jan.ahmed.prg@gmail.com>
2024-03-03 11:17:44 -0500

    css maxxing

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,7 @@ const Home = () => {
   const [studySetName, setStudySetName] = useState("");
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [cardSets, setCardSets] = useState([]);
+  const [selectedCard, setSelectedCard] = useState([]);
 
   const [credentials, setCredentials] = useState(
     localStorage.getItem("credentials")
@@ -16,10 +17,14 @@ const Home = () => {
   const [currUserId, setCurrUserId] = useState(
     localStorage.getItem("currUserId")
   );
+  const [currentCardIndex, setCurrentCardIndex] = useState(0);
+  const [isFlipped, setIsFlipped] = useState(false);
 
   useEffect(() => {
     setCredentials(localStorage.getItem("credentials"));
     setCurrUserId(localStorage.getItem("currUserId"));
+    handleFetch();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   //   const fetchCardSets = () => {
@@ -37,28 +42,9 @@ const Home = () => {
 
   const handleRemoveFile = () => {
     setSelectedFile(null);
-    // Reset the input element's value
-    document.getElementById("fileInput").value = "";
+    setStudySetName("");
   };
 
-  const handleFetch = () => {
-    fetch("http://34.42.246.209:5000/user/65e4582acaa3e72677525db8/sets", {
-      method: "Get",
-    })
-      .then((response) => {
-        if (response.ok) {
-          console.log("File uploaded successfully");
-        } else {
-          console.error("Failed to upload file");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setIsSubmitting(false);
-        console.log(data);
-        setCardSets(data);
-      });
-  };
   const handleFetch = () => {
     fetch(`http://34.42.246.209:5000/user/${currUserId}/sets`, {
       method: "Get",
@@ -113,6 +99,32 @@ const Home = () => {
     }
   };
 
+  const handleCard = (index) => {
+    setSelectedCard(cardSets[index].flashcards);
+    setCurrentCardIndex(0);
+    setIsFlipped(false);
+  };
+
+  const handleNextCard = () => {
+    setIsFlipped(false); // Reset flip state before moving to the next card
+
+    if (currentCardIndex < selectedCard.length - 1) {
+      setCurrentCardIndex(currentCardIndex + 1);
+    } else {
+      setCurrentCardIndex(0); // Loop back to the first card
+    }
+  };
+
+  const handlePrevCard = () => {
+    setIsFlipped(false); // Reset flip state before moving to the previous card
+
+    if (currentCardIndex > 0) {
+      setCurrentCardIndex(currentCardIndex - 1);
+    } else {
+      setCurrentCardIndex(selectedCard.length - 1); // Go to the last card when at the beginning
+    }
+  };
+
   return (
     <div className="container-home">
       <div>
@@ -132,7 +144,6 @@ const Home = () => {
                 borderRadius: "20px",
               }}
               type="file"
-              placeholder="Upload File"
               onChange={handleFileChange}
             />
             <Input
@@ -151,6 +162,7 @@ const Home = () => {
               type="text"
               placeholder="Enter a title for your study set"
               fontSize={"1px"}
+              lineHeight={"50px"}
               _placeholder={{ color: "white" }}
               value={studySetName}
               onChange={(e) => setStudySetName(e.target.value)}
@@ -195,23 +207,6 @@ const Home = () => {
               >
                 Submit
               </Button>
-              <Button
-                style={{
-                  marginLeft: "5%",
-                  marginTop: "20px",
-                  width: "20%",
-                  height: "50px",
-                  fontSize: "150%",
-                  borderRadius: "20px",
-                }}
-                colorScheme="blue"
-                onClick={handleFetch}
-                // disabled={
-                //   !selectedFile || studySetName.trim() === "" || isSubmitting
-                // }
-              >
-                Refresh
-              </Button>
             </div>
           )}
         </ChakraProvider>
@@ -224,6 +219,7 @@ const Home = () => {
             borderColor: "#f15c58",
             borderWidth: "3px",
             borderRadius: "30px",
+            display: "flex",
           }}
         >
           <h1
@@ -232,10 +228,28 @@ const Home = () => {
               margin: "0",
               color: "white",
               fontWeight: "bold",
+              width: "100%",
             }}
           >
             Your Study Sets
           </h1>
+          <div style={{ width: "100%", textAlign: "right" }}>
+            <Button
+              style={{
+                width: "50%",
+                fontSize: "150%",
+                borderRadius: "20px",
+                padding: "30px",
+              }}
+              colorScheme="blue"
+              onClick={handleFetch}
+              // disabled={
+              //   !selectedFile || studySetName.trim() === "" || isSubmitting
+              // }
+            >
+              Refresh
+            </Button>
+          </div>
         </div>
       </div>
       <div
@@ -250,11 +264,86 @@ const Home = () => {
         {cardSets ? (
           cardSets.map((cardSet, index) => (
             <div key={index}>
-              <Button colorScheme="blue">{cardSet.set_name}</Button>
+              <Button
+                style={{
+                  marginTop: "10px",
+                  marginBottom: "10px",
+                  fontSize: "1.6rem",
+                  padding: "30px",
+                  borderRadius: "20px",
+                }}
+                colorScheme="blue"
+                onClick={() => handleCard(index)}
+              >
+                {cardSet.set_name}
+              </Button>
             </div>
           ))
         ) : (
           <h1>No card sets found</h1>
+        )}
+      </div>
+      <div
+        style={{
+          borderColor: "#f15c58",
+          borderWidth: "3px",
+          marginTop: "30px",
+          borderRadius: "30px",
+          padding: "30px",
+        }}
+      >
+        {selectedCard.length > 0 && (
+          <div className="card-container">
+            <h2 style={{ fontSize: "1.6rem" }}>Here are your flashcards!</h2>
+            <div
+              className={`card ${isFlipped ? "flipped" : ""}`}
+              onClick={() => setIsFlipped(!isFlipped)}
+              style={{ color: "black", fontSize: "1.3rem", marginTop: "20px" }}
+            >
+              <div className="card-inner">
+                <div
+                  className="card-front"
+                  style={{
+                    padding: "20px",
+                    paddingTop: "10px",
+                    paddingBottom: "10px",
+                    overflowY: "scroll",
+                  }}
+                >
+                  <h3>Card {currentCardIndex + 1}</h3>
+                  <p>Question: {selectedCard[currentCardIndex].term}</p>
+                </div>
+                <div
+                  className="card-back"
+                  style={{
+                    padding: "20px",
+                    paddingTop: "10px",
+                    paddingBottom: "10px",
+                    overflowY: "scroll",
+                  }}
+                >
+                  <h3>Answer:</h3>
+                  <p>{selectedCard[currentCardIndex].description}</p>
+                </div>
+              </div>
+            </div>
+            <Button
+              style={{ margin: "20px" }}
+              colorScheme="whiteAlpha"
+              className="button back-button"
+              onClick={handlePrevCard}
+            >
+              Back
+            </Button>
+            <Button
+              style={{ margin: "20px" }}
+              colorScheme="whiteAlpha"
+              className="button next-button"
+              onClick={handleNextCard}
+            >
+              Next
+            </Button>
+          </div>
         )}
       </div>
     </div>
